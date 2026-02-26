0a569dd767ddee4e75e67e1d71f0fac34323ebf9
Sushilk <sushilkamble7738@gmail.com>
2026-02-26 13:07:39 +0530

    Practiced mixins and generics APIview of DRF concepts

--- Infotech/api/views.py
+++ Infotech/api/views.py
@@ -7,7 +7,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from django.http import Http404
-
+from rest_framework import mixins, generics
 
 
 
@@ -65,41 +65,92 @@ from django.http import Http404
     
 #* class based view
 
-class EmpInfo(APIView):
-    def get(self,request):
-        employees = Employee.objects.all()
-        serializer = EmployeeSerializer(employees, many = True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+# class EmpInfo(APIView):
+#     def get(self,request):
+#         employees = Employee.objects.all()
+#         serializer = EmployeeSerializer(employees, many = True)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request):
-        serializer = EmployeeSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        raise Response(status=status.HTTP_400_BAD_REQUEST)
+#     def post(self, request):
+#         serializer = EmployeeSerializer(data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         raise Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-class EmpDetails(APIView):
-    def get_object(self, pk):
-        try:
-            return Employee.objects.get(pk=pk)
-        except Employee.DoesNotExist:
-            raise Http404
+# class EmpDetails(APIView):
+#     def get_object(self, pk):
+#         try:
+#             return Employee.objects.get(pk=pk)
+#         except Employee.DoesNotExist:
+#             raise Http404
 
-    def get(self, request, pk):
-        employees = self.get_object(pk)
-        serializer = EmployeeSerializer(employees)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+#     def get(self, request, pk):
+#         employees = self.get_object(pk)
+#         serializer = EmployeeSerializer(employees)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def put(self, request, pk):
-        employees = self.get_object(pk)
-        serializer = EmployeeSerializer(employees, data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+#     def put(self, request, pk):
+#         employees = self.get_object(pk)
+#         serializer = EmployeeSerializer(employees, data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk):
-        employees = self.get_object(pk)
-        employees.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     def delete(self, request, pk):
+#         employees = self.get_object(pk)
+#         employees.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+# =============================================================================================
+                            #*mixins
+# =============================================================================================
+
+# class EmpInfo(mixins.ListModelMixin, mixins.CreateModelMixin,generics.GenericAPIView):
+#     queryset = Employee.objects.all()
+#     serializer_class = EmployeeSerializer
+
+#     def get(self, request):
+#         return self.list(request)
+
+#     def post(self, request):
+#         return self.create(request)
+
+
+# class EmpDetails(mixins.RetrieveModelMixin, mixins.UpdateModelMixin,
+#                 mixins.DestroyModelMixin ,generics.GenericAPIView):
+
+#     queryset = Employee.objects.all()
+#     serializer_class = EmployeeSerializer
+
+#     def get(self, request, pk):
+#         return self.retrieve(request, pk)
+
+#     def put(self, request, pk):
+#         return self.update(request, pk)
+
+#     def delete(self, request, pk):
+#         return self.destroy(request, pk)
+
+
+
+# =============================================================================================
+                            #*generics APIView
+# =============================================================================================
+
+
+class EmpInfo(generics.ListCreateAPIView ,generics.GenericAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+class EmpDetails(generics.RetrieveUpdateDestroyAPIView ,generics.GenericAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    lookup_field = "pk"
+    
--- api/views.py
+++ api/views.py
@@ -7,6 +7,7 @@ from rest_framework import status
 
 from rest_framework.views import APIView
 from django.http import Http404
+from rest_framework import generics, mixins
 
 
 # @api_view(['GET', 'POST'])
@@ -52,42 +53,89 @@ from django.http import Http404
 
 
 
-class DocView(APIView):
-    def get(self, request):
-        doctors = Doctors.objects.all()
-        serializer = DoctorSerializer(doctors, many = True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+# class DocView(APIView):
+#     def get(self, request):
+#         doctors = Doctors.objects.all()
+#         serializer = DoctorSerializer(doctors, many = True)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request):
-        serializer = DoctorSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def post(self, request):
+#         serializer = DoctorSerializer(data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
-class DocDetails(APIView):
-    def get_object(self, pk):
-        try:
-            return Doctors.objects.get(pk=pk)
-        except Doctors.DoesNotExist:
-            raise Http404
+# class DocDetails(APIView):
+#     def get_object(self, pk):
+#         try:
+#             return Doctors.objects.get(pk=pk)
+#         except Doctors.DoesNotExist:
+#             raise Http404
 
-    def get(self, request, pk):
-        doctors = self.get_object(pk)
-        serializer = DoctorSerializer(doctors)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+#     def get(self, request, pk):
+#         doctors = self.get_object(pk)
+#         serializer = DoctorSerializer(doctors)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
     
-    def put(self, request, pk):
-        doctors = self.get_object(pk)
-        serializer = DoctorSerializer(doctors, data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+#     def put(self, request, pk):
+#         doctors = self.get_object(pk)
+#         serializer = DoctorSerializer(doctors, data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk):
-        doctors = self.get_object(pk)
-        doctors.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     def delete(self, request, pk):
+#         doctors = self.get_object(pk)
+#         doctors.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+# ==========================================================================================
+                                #* mixins
+# ==========================================================================================
+
+# class DocView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+#     queryset = Doctors.objects.all()
+#     serializer_class = DoctorSerializer
+
+#     def get(self, request):
+#         return self.list(request)
+
+#     def post(self, request):
+#         return self.create(request)
+    
+
+
+# class DocDetails(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, generics.GenericAPIView):
+#     queryset = Doctors.objects.all()
+#     serializer_class = DoctorSerializer
+
+#     def get(self, request, pk):
+#         return self.retrieve(request, pk)
+
+#     def put(self, request, pk):
+#         return self.update(request, pk)
+
+    
+#     def delete(self, request, pk):
+#         return self.delete(request, pk  )
+
+
+# ==========================================================================================        
+                            #*generics APIView
+# ==========================================================================================        
+
+
+class DocView(generics.ListCreateAPIView):
+    queryset = Doctors.objects.all()
+    serializer_class = DoctorSerializer
+
+class DocDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Doctors.objects.all()
+    serializer_class = DoctorSerializer
+    lookup_field = "pk"
